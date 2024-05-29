1a220f95017795e8744842d956f368c75ee44a35
a1688467 <a1688467@student.adelaide.edu.au>
2024-05-29 18:52:44 +0930

    Dump before refactor

--- R/fit_simple_mixed_model.R
+++ R/fit_simple_mixed_model.R
@@ -1,13 +1,37 @@
-fit_simple_mixed_model <- function(data) {
-  fit <- lmer(gene_expression ~ cell_line+treatment+conc+(1|name), data = data)
+fit_simple_mixed_model <- function(cleaned_data) {
+  fit <- lmer(gene_expression ~ treatment:conc+treatment:cell_line+conc+cell_line+treatment+(1|name), data = cleaned_data)
   return (fit)
 }
 
 
 
 # # Driver
-# pacman::p_load(tidyverse,targets, gglm, lme4)
-# d_file <- tar_load(raw_data_file)
-# df <- clean_data(d_file)
-# m <- fit_model(df)
+pacman::p_load(tidyverse,targets, gglm, lme4, lmerTest, pbkrtest)
+tar_load(cleaned_data)
+m <- fit_simple_mixed_model(cleaned_data)
 
+(aov <- anova(m))
+show_tests(aov, fractions = TRUE)$Product
+if(requireNamespace("pbkrtest", quietly = TRUE))
+  anova(m, type=2, ddf="Kenward-Roger")
+
+ranova(model = m)
+
+drop1(m)
+
+(lsm <- ls_means(m))
+#ls_means(m, which = "Product", pairwise = TRUE)
+
+step_result <- step(m)
+step_result
+
+final_model <- get_model(step_result)
+summary(final_model)
+
+ktm <- step(m,keep = "treatment")
+summary(get_model(ktm))
+
+fm <- lmer(gene_expression ~ conc + (1 | name) + conc:treatment, data = cleaned_data)
+summary(fm)
+
+AIC(fm, final_model)
--- R/get_prediction_interval_plot.R
+++ R/get_prediction_interval_plot.R
@@ -1,9 +1,50 @@
 get_prediction_interval_plot <- function(model, data) {
+  new <- data
+  new %>%
+    mutate(pred = predict(model, re.form = NULL)) %>% #compute mean predictions form mixed effects model
+    ggplot(aes(nap, lrichness, col = name)) + #plot the data
+    geom_point(show.legend = FALSE) +
+    geom_line(aes(nap, pred), show.legend = FALSE) + #plot the mixed effects model predictions
+    facet_wrap(~name) +
+    theme_bw()
+  make_individual_preds <- function(model, new){
+    # predict function for bootstrapping
+    predfn <- function(.) {
+      predict(., newdata=new, re.form=NULL)
+    }
+    # summarise output of bootstrapping
+    sumBoot <- function(merBoot) {
+      return(
+        data.frame(model = apply(merBoot$t, 2, function(x) as.numeric(quantile(x, probs=.5, na.rm=TRUE))),
+                   lwr = apply(merBoot$t, 2, function(x) as.numeric(quantile(x, probs=.025, na.rm=TRUE))),
+                   upr = apply(merBoot$t, 2, function(x) as.numeric(quantile(x, probs=.975, na.rm=TRUE)))
+        )
+      )
+    }
+    #get samples
+    boot <- lme4::bootMer(model, predfn, nsim=500, use.u=TRUE, type="parametric")
+    #save predictions in the tibble
+    preds <- new %>%
+      bind_cols(sumBoot(boot))
+    #return the predictions
+
+    return(preds)
+  }
+  new_individual_preds <- make_individual_preds(model,new)
+
+  new_individual_preds %>%
+    ggplot(aes(nap, lrichness, col = name, fill = name)) +
+    geom_point(show.legend = FALSE) +
+    geom_line(aes(nap, model), show.legend = FALSE) +
+    geom_ribbon(aes(ymin = lwr, ymax = upr), alpha = 0.3, show.legend = FALSE) +
+    facet_wrap(~name) +
+    theme_bw()
+
 
 }
 
 # Driver
-#pacman::p_load(tidyverse,targets, lme4)
-#tar_load(cleaned_data)
-#tar_load(fitted_model)
-#get_prediction_interval_plot(fitted_model, cleaned_data)
+pacman::p_load(tidyverse,targets, lme4)
+tar_load(cleaned_data)
+tar_load(fitted_model)
+get_prediction_interval_plot(fitted_model, cleaned_data)
