df7b1f0587cb418fc5168515035584b768806c37
a1688467 <a1688467@student.adelaide.edu.au>
2024-06-12 21:24:41 +0930

    Remove old figures and add note to targets

--- _targets.R
+++ _targets.R
@@ -10,17 +10,22 @@ tar_option_set(
 tar_source()
 
 list(
+  # IMPORTANT
+  # If you change these two lines, i.e. for new data then make sure you change
+  # clean_data's code bellow the function
   tar_file(raw_data_file, "raw-data/karl-data-4-3-24.xlsx"),
   tar_target(cleaned_data, clean_data(raw_data_file)),
 
+  # Both part of Karls requests
   tar_target(eda, run_EDA(cleaned_data)),
   tar_target(karls_plot, create_karls_plot(cleaned_data)),
 
-
+  # All interaction models
   tar_target(simple_model, fit_simple_model(cleaned_data)),
   tar_target(twoway_model, fit_twoway_model(cleaned_data)),
   tar_target(threeway_model, fit_threeway_model(cleaned_data)),
 
+  # Validate the best
   tar_target(AIC_table, create_AIC_table(simple_model, twoway_model, threeway_model)),
 
   # threeway is the best
