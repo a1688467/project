bda488e1e4f37e8c9af1e018ccb69208e01d1a8f
a1688467 <a1688467@student.adelaide.edu.au>
2024-06-13 21:57:39 +0930

    Format this code better

--- R/create_interaction_plot.R
+++ R/create_interaction_plot.R
@@ -2,13 +2,15 @@
 # Create interaction plot
 # Origionally requested by Karl
 # We also use this for the report
-#
+# geom_label_repel code is horrible and mostly based of trial and error
+# to get it to look the same as Jono's
 #
 create_interaction_plot <- function(raw_data) {
+  # Colours taken from Jono's plot
   col_pal <- c("#78a8d1", "#d5bf98")
-  #tiff(filename = "plot.tiff", width = 9, height = 6, units = "in", res = 500)
 
-  pWild <- raw_data %>% filter(cell_line == "Wild-type") |>
+  # Wild-type plot
+  wild_plot <- raw_data %>% filter(cell_line == "Wild-type") |>
     ggplot(aes(conc, gene_expression, col = Treatment)) +
     geom_point(
       color = 'black',
@@ -20,19 +22,17 @@ create_interaction_plot <- function(raw_data) {
     geom_label_repel(
       data = raw_data %>% filter(conc == 10 &
                                    cell_line == "Wild-type"),
-      # Filter data first
       aes(label = name, fill = Treatment),
       show.legend = FALSE,
-      #ylim =c(0,60),
       xlim = c(10.15, 12.1),
       nudge_x = -0.5,
       colour = "black",
       segment.colour = "black",
       min.segment.length = 0.1,
-      #hjust = 1,
     ) +  scale_color_manual(values = col_pal)
 
-  p101 <- raw_data %>% filter(cell_line != "Wild-type") |>
+  # Cell-type 101 plot
+  type101_plot <- raw_data %>% filter(cell_line != "Wild-type") |>
     ggplot(aes(conc, gene_expression, col = Treatment)) +
     geom_point(
       color = 'black',
@@ -44,21 +44,21 @@ create_interaction_plot <- function(raw_data) {
     geom_label_repel(
       data = raw_data %>% filter(conc == 10 &
                                    cell_line != "Wild-type"),
-      # Filter data first
       aes(label = name, fill = Treatment),
       show.legend = FALSE,
-      #ylim =c(0,60),
       xlim = c(10, 17.27),
       colour = "black",
       segment.colour = "black",
       nudge_x = 1.5,
       nudge_y = 0.05,
       min.segment.length = 0.1,
+    ) + scale_color_manual(values = col_pal)
 
-    ) +  scale_color_manual(values = col_pal)
 
-
-  combined <- pWild + p101 + plot_annotation(tag_levels = 'A') &
+  # Combined plot
+  # plot_layout was really finicky, so it's been split off
+  # might work on older/newer versions of R but this works now
+  combined_initial <- wild_plot + type101_plot + plot_annotation(tag_levels = 'A') &
     labs(y = "Gene Expression", x = "ug/ml", col = "Treatment") &
     theme_bw() &
     harrypotter::scale_fill_hp_d("Ravenclaw") &
@@ -69,15 +69,16 @@ create_interaction_plot <- function(raw_data) {
     scale_fill_manual(values = col_pal) &
     scale_color_manual(values = col_pal)
 
-  cc <- combined + plot_layout(guides = "collect") + labs(col = "Treatment") +  scale_fill_manual(values = col_pal) + scale_color_manual(values = col_pal)
+  combined_final <- combined_initial + plot_layout(guides = "collect") +
+    labs(col = "Treatment") +  scale_fill_manual(values = col_pal) +
+    scale_color_manual(values = col_pal)
 
+  # Save the final plot!
   ggsave(filename = here::here("figs/interactionPlot.svg"),
-         plot = cc)
-
+         plot = combined_final)
 }
 
-#
-# # Driver
+# Driver
 # pacman::p_load(tidyverse, targets, showtext, ggrepel, patchwork, svglite)
 # tar_load(cleaned_data)
 # create_interaction_plot(cleaned_data)
